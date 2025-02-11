fa7a773e50415d71f0dd2ce790872f2ba78885a8
Dipendra Shah <83157331+dipendrakshah@users.noreply.github.com>
2025-02-11 17:19:21 +0545

    Changes 202

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,3 +1,22 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+  });
+  
+  const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+  });
+  
+  export const metadata: Metadata = {
+    title: "Dipendra K. Shah",
+    description: "I am Dipendra",
+  };
+
 export default function RootLayout({
     children,
   }: Readonly<{
@@ -5,7 +24,7 @@ export default function RootLayout({
   }>) {
     return (
       <html lang="en">
-        <body>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
         </body>
       </html>
